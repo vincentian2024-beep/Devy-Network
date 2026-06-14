27c9b509edf32acc9a7826919768ce589ecc1b61
vincentian2024-beep <vincentian2024@gmail.com>
2026-06-14 10:08:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+require('dotenv').config();
 
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
